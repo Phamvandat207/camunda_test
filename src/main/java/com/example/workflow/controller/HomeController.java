5165a01e2a6f3fe5e857b41d5b52a38f738afd4c
Phamvandat207 <phamvandat207@gmail.com>
2022-05-12 11:05:46 +0700

    add new demo (ScriptTask, ServiceTask)

--- src/main/java/com/example/workflow/controller/HomeController.java
+++ src/main/java/com/example/workflow/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.example.workflow.controller;
 
+import com.example.workflow.model.CamundaProcessDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,8 @@ public class HomeController {
     }
 
     @PostMapping("create")
-    public String startProcess(){
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-project-process");
+    public String startProcess(@RequestBody CamundaProcessDTO camundaProcessDTO){
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(camundaProcessDTO.getProcessKey());
         return processInstance.getProcessInstanceId();
     }
 }
---
+++ src/main/java/com/example/workflow/model/CamundaProcessDTO.java
@@ -0,0 +1,12 @@
+package com.example.workflow.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CamundaProcessDTO {
+    private String processKey;
+}
